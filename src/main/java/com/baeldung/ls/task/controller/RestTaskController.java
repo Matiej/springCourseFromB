ac4b68d821d6dd4bd4ba1e61cd0613be14035637
Maciej Wójcik <maciek.wojcik179@gmail.com>
2023-07-23 22:37:14 +0200

    added task add method

---
+++ src/main/java/com/baeldung/ls/task/controller/RestTaskController.java
@@ -0,0 +1,53 @@
+package com.baeldung.ls.task.controller;
+
+import com.baeldung.ls.global.headerfactory.HttpHeadersFactory;
+import com.baeldung.ls.project.controller.ProjectDto;
+import com.baeldung.ls.task.application.TaskService;
+import com.baeldung.ls.task.domain.Task;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@Validated
+@RequestMapping("/api/tasks")
+public class RestTaskController {
+    private static final String TASKS_PATH = "api/tasks";
+
+    private final TaskService taskService;
+
+    public RestTaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping(consumes = APPLICATION_JSON_VALUE, value = "/{id}")
+    ResponseEntity<TaskDto> addTaskToProject(@PathVariable("id") @NotNull(message = "Project ID variable can't be null")
+                                                @Min(value = 1, message = "Project ID variable mu be greater then zero") Long id,
+                                                @Valid @RequestBody RestCreateTaskCommand command) {
+        Task savedTask = taskService.save(command.toCreateTaskCommand(id));
+        TaskDto taskDto = TaskDto.convertToTaskDto(savedTask);
+
+        return ResponseEntity.created(getUri(savedTask.getId()))
+                .headers(HttpHeadersFactory.getSuccessfulDefaultHeaders(HttpStatus.CREATED, HttpMethod.POST))
+                .build();
+    }
+
+    private static URI getUri(Long id) {
+        return ServletUriComponentsBuilder
+                .fromCurrentServletMapping()
+                .path(TASKS_PATH)
+                .path("/{id}")
+                .buildAndExpand(id)
+                .toUri();
+    }
+}
---
+++ src/main/java/com/baeldung/ls/task/controller/TaskDto.java
@@ -0,0 +1,120 @@
+package com.baeldung.ls.task.controller;
+
+import com.baeldung.ls.task.domain.Task;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+//todo neeed lombok!
+public class TaskDto {
+
+    private Long id;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastUpdatedAt;
+    private String name;
+    private String description;
+    private LocalDate dueDate;
+    private String status;
+
+    public TaskDto(Long id, LocalDateTime createdAt, LocalDateTime lastUpdatedAt, String name, String description, LocalDate dueDate, String status) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.lastUpdatedAt = lastUpdatedAt;
+        this.name = name;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getLastUpdatedAt() {
+        return lastUpdatedAt;
+    }
+
+    public void setLastUpdatedAt(LocalDateTime lastUpdatedAt) {
+        this.lastUpdatedAt = lastUpdatedAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TaskDto taskDto = (TaskDto) o;
+        return Objects.equals(id, taskDto.id) && Objects.equals(createdAt, taskDto.createdAt) && Objects.equals(lastUpdatedAt, taskDto.lastUpdatedAt) && Objects.equals(name, taskDto.name) && Objects.equals(description, taskDto.description) && Objects.equals(dueDate, taskDto.dueDate) && Objects.equals(status, taskDto.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, createdAt, lastUpdatedAt, name, description, dueDate, status);
+    }
+
+    @Override
+    public String toString() {
+        return "TaskDto{" +
+                "id=" + id +
+                ", createdAt=" + createdAt +
+                ", lastUpdatedAt=" + lastUpdatedAt +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", dueDate=" + dueDate +
+                ", status='" + status + '\'' +
+                '}';
+    }
+
+    public static TaskDto convertToTaskDto(Task task) {
+        return new TaskDto(task.getId(),
+                task.getCreatedAt(),
+                task.getLastUpdatedAt(),
+                task.getName(),
+                task.getDescription(),
+                task.getDueDate(),
+                task.getStatus().name());
+    }
+}
