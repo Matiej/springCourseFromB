871883097cd530d6c36c68fbb9f9b6fc5b8c0e4d
Maciej Wójcik <maciek.wojcik179@gmail.com>
2023-07-29 22:02:18 +0200

    add basic security and admin controller - tbc - security needs to be configure better

---
+++ src/main/java/com/baeldung/ls/admin/controller/AdminController.java
@@ -0,0 +1,21 @@
+package com.baeldung.ls.admin.controller;
+
+import com.baeldung.ls.global.headerfactory.HttpHeadersFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    @GetMapping(value = "/welcome")
+    ResponseEntity<?> welcome() {
+        return ResponseEntity.ok()
+                .headers(HttpHeadersFactory.getSuccessfulDefaultHeaders(HttpStatus.OK, HttpMethod.GET))
+                .body("WELCOME TO TEST APP. LOGIN PROBABLY WITH SUCCESS");
+    }
+}
